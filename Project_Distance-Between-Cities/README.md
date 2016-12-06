5a8df08c21605ce6ab2a72d37cc00deb11fca085
Juan Oliveira <juanplaystation82@gmail.com>
2016-12-06 11:41:06 -0200

    Atualizado

---
+++ Project_Distance-Between-Cities/README.md
@@ -0,0 +1,2 @@
+# cpp-projects
+Projects in C++
--- Project_Distance-Between-Cities/main.cpp
+++ Project_Distance-Between-Cities/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <cmath>
 #include "split.hpp"
 
 using namespace std;
@@ -11,89 +12,93 @@ void abreArquivoCEP();
 void abreArquivoMunicip();
 
 int main(){
-    string linha1, linha2, cep1City, cep1CityEstate, cep2City, cep2CityEstate;
-
+    string linha,cep1City,cep1CityEstate,cep2City,cep2CityEstate,City1Latitude,City1Longitude,City2Latitude,City2Longitude;
     int cep1, cep2;
     bool findCEP = false;
-
-    cout<<"\n  CEP 1: "<<endl;
+    cout<<"\n\tDistance Between Cities"<<endl;
+    cout<<"\n  CEP 1: ";
     cin>>cep1;
-    cout<<"\n  CEP 2: "<<endl;
+    cout<<"\n  CEP 2: ";
     cin>>cep2;
 
     abreArquivoCEP();
     while (arquivoCep.good()){
-        getline(arquivoCep, linha1);
-        if (linha1.size()==0)
+        getline(arquivoCep, linha);
+        if (linha.size()==0)
             continue;
-        vector<string> vCEP=split(linha1, ';');
-        int cepCompareStart = atoi(vCEP[1].c_str());
-        int cepCompareEnd = atoi(vCEP[2].c_str());
+        vector<string> v=split(linha, ';');
+        int cepCompareStart = atoi(v[1].c_str());
+        int cepCompareEnd = atoi(v[2].c_str());
         if(cep1 >= cepCompareStart && cep1 <= cepCompareEnd){
-            cep1City = vCEP[0];
-            cep1CityEstate = vCEP[0]+"-"+vCEP[3];
+            cep1City = v[0];
+            cep1CityEstate = v[0]+"-"+v[3];
             findCEP = true;
         }
     }
     if (!findCEP){
         cout << "Nenhuma cidade foi encontrada com esse CEP." << endl;
     }arquivoCep.close();
-
     abreArquivoCEP();
     while (arquivoCep.good()){
-        getline(arquivoCep, linha1);
-        if (linha1.size()==0)
+        getline(arquivoCep, linha);
+        if (linha.size()==0)
             continue;
-        vector<string> vCEP=split(linha1, ';');
-        int cepCompareStart = atoi(vCEP[1].c_str());
-        int cepCompareEnd = atoi(vCEP[2].c_str());
+        vector<string> v=split(linha, ';');
+        int cepCompareStart = atoi(v[1].c_str());
+        int cepCompareEnd = atoi(v[2].c_str());
         if(cep2 >= cepCompareStart && cep2 <= cepCompareEnd){
-            cep2City = vCEP[0];
-            cep2CityEstate = vCEP[0]+"-"+vCEP[3];
+            cep2City = v[0];
+            cep2CityEstate = v[0]+"-"+v[3];
             findCEP = true;
         }
     }
     if (!findCEP){
         cout << "Nenhuma cidade foi encontrada com esse CEP." << endl;
     }arquivoCep.close();
-
-    /*
-    ele vai procurar ASB
-    qnd encontrar tem isso aq:
-    4731;-22,88;-49,23;AGUAS DE SANTA BARBARA;SP
-    ID;LATITUDE;LONGITUDE;MUNICIPIO;UF
-    0    1        2            3    4
-    */
-
     abreArquivoMunicip();
     while (arquivoMunicip.good()){
-        getline(arquivoMunicip, linha2);
-        if (linha2.size()==0)
+        getline(arquivoMunicip, linha);
+        if (linha.size()==0)
             continue;
-        vector<string> vMunicip=split(linha2, ';');
-        if(cep1City == vMunicip[3]){
-            cout<<"\nEncontrou a cidade 1\n";
+        vector<string> v=split(linha, ';');
+        if(cep1City == v[3]){
+            City1Latitude = v[1];
+            City1Longitude = v[2];
+            break;
+        }
+    }arquivoMunicip.close();
+    abreArquivoMunicip();
+    while (arquivoMunicip.good()){
+        getline(arquivoMunicip, linha);
+        if (linha.size()==0)
+            continue;
+        vector<string> v=split(linha, ';');
+        if(cep2City == v[3]){
+            City2Latitude = v[1];
+            City2Longitude = v[2];
             break;
         }
     }arquivoMunicip.close();
 
-    abreArquivoMunicip();
-    while (arquivoMunicip.good()){
-        getline(arquivoMunicip, linha2);
-        if (linha2.size()==0)
-            continue;
-        vector<string> vMunicip=split(linha2, ';');
-        if(cep2City == vMunicip[3]){
-            cout<<"\nEncontrou a cidade 2\n";
-            break;
-        }
-    }arquivoMunicip.close();
+    cout<<"\nCIDADE 1: "<<cep1City<<endl;
+    cout<<"LATITUDE: "<<City1Latitude<<"\nLONGITUDE: "<<City1Longitude<<endl;
 
-    cout <<cep1CityEstate<<" -> "<<cep2CityEstate<<endl;
+    cout<<"\nCIDADE 2: "<<cep2City<<endl;
+    cout<<"LATITUDE: "<<City2Latitude<<"\nLONGITUDE: "<<City2Longitude<<endl;
+
+    /* conta */
+    DIST (A, B) = R ∗ acos(α + β ∗ δ);
+
+    α = sen(City1Latitude) ∗ sen(City2Latitude);
+    β = cos(City1Latitude) ∗ cos(City2Latitude);
+    δ = cos(A.lon − B.lon);
+
+    R = 6372, 795477598;
+
+    cout<<"\n"<<cep1CityEstate<<" -> "<<cep2CityEstate<<endl;
 
     return 0;
 }
-
 void abreArquivoCEP(){
     string cepCsv = "cep.csv";
     arquivoCep.open(cepCsv.c_str(), ios::in);
